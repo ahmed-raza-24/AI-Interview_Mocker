06720554e7a459cfa6a5dc0ee59e8d33b9185eba
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-05-12 20:03:38 +0530

    Adding NavBar / Header

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
-function Dashoard() {
+function Dashboard() {
   return (
     <div>Dashoard
 
@@ -11,4 +11,4 @@ function Dashoard() {
   )
 }
 
-export default Dashoard
+export default Dashboard
