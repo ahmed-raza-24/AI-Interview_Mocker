54dac5da35bfec63fb349af959ba5d074cadcad3
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-06-05 10:18:36 +0530

    interviewId details & fetching details from db

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,14 +1,33 @@
 "use client"
-import React, { useEffect } from 'react'
-
+import {db} from '@/utiles/db'
+import {eq} from 'drizzle-orm'
+import { MockInterview } from '@/utiles/schema'
+import React, { useEffect, use } from 'react'
+import { useState } from 'react'
 function Interview({params}) {
+    const { interviewId } = use(params)  // unwrap karo
 
+    const [interviewData, setInterviewData] = useState()
     useEffect(()=>{
-        console.log(params)
+        console.log(interviewId)
+        GetInterviewDetails();
     },[])
 
+    /*
+    used to get interview details by mockId/interview Id
+     */
+    const GetInterviewDetails=async()=>{
+      const result=await db.select().from(MockInterview)
+      .where(eq(MockInterview.mockId, interviewId))
+
+      console.log(result);
+      setInterviewData(result[0]);
+    }
+
   return (
-    <div>Interview</div>
+    <div className='my-10 flex justify-center flex-col items-center'>
+      <h2 className='font-bold text-2xl'>Let's Get Started</h2>
+    </div>
   )
 }
 
