955e1d6062823f94288225becd58064a8c4da4cc
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-06-08 10:17:15 +0530

    QuestionsSection && updated interviewId code

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -3,32 +3,41 @@ import { db } from '@/utiles/db';
 import { eq } from 'drizzle-orm'
 import { MockInterview } from '@/utiles/schema';
 import React, { useEffect, useState, use } from 'react'
+import QuestionsSection from './_components/QuestionsSection';
 
-function StartInterview({params}) {
-    const { interviewId } = use(params);
+function StartInterview({ params }) {
+  const { interviewId } = use(params);
 
-    const [interviewData, setInterviewData] = useState();
-    const [mockInterviewQuestion, setmockInterviewQuestion] = useState();
-    
-    useEffect(()=>{
-        GetInterviewDetails()
-    }, [interviewId]);
+  const [interviewData, setInterviewData] = useState();
+  const [mockInterviewQuestion, setmockInterviewQuestion] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  
+  useEffect(() => {
+    GetInterviewDetails();
+    console.log(typeof mockInterviewQuestion)
+    console.log(mockInterviewQuestion)
+  }, [interviewId]);
 
   /*
   used to get interview details by mockId/interview Id
    */
   const GetInterviewDetails = async () => {
     const result = await db.select().from(MockInterview)
-      .where(eq(MockInterview.mockId,interviewId))
+      .where(eq(MockInterview.mockId, interviewId))
 
-    const jsonMockResp=JSON.parse(result[0].jsonMockResp)
+    const jsonMockResp = JSON.parse(result[0].jsonMockResp)
     console.log(jsonMockResp)
     setmockInterviewQuestion(jsonMockResp);
     setInterviewData(result[0]);
   }
 
   return (
-    <div>StartInterview</div>
+    <div>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        {/*Questions */}
+        <QuestionsSection mockInterviewQuestion={mockInterviewQuestion} />
+      </div>
+    </div>
   )
 }
 
