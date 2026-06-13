886312e4639b1c32871b1326bb870fe380c66c02
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-06-13 11:44:16 +0530

    updated feedback page && npm collapsible component

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client"
+import { db } from '@/utiles/db';
+import { UserAnswer } from '@/utiles/schema'
+import { eq } from 'drizzle-orm';
+import React, { useEffect, useState, use } from 'react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronsUpDown } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+
+
+const Feedback = ({ params }) => {
+
+  const { interviewId } = use(params);
+  const [feedbackList, setFeedbackList] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    GetFeedback();
+  }, [])
+
+  const GetFeedback = async () => {
+    const result = await db.select()
+      .from(UserAnswer)
+      .where(eq(UserAnswer.mockIdRef, interviewId))
+      .orderBy(UserAnswer.id);
+
+    console.log(result);
+    setFeedbackList(result);
+  }
+
   return (
-    <div>feedback</div>
+    <div className='p-10'>
+      <h2 className='text-2xl font-bold text-green-500'>Congratulations</h2>
+      <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+      <h2 className='text-blue-700 text-lg my-3'>Your overall interview rating: <strong>7/10</strong></h2>
+
+      <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, Your answer and feedback for improvement</h2>
+      {feedbackList && feedbackList.map((item, index) => (
+        <Collapsible  key={index} className='mt-7'>
+          <CollapsibleTrigger className='p-2 bg-secondary rounded-lg flex justify-between my-2 text-left gap-7 w-full'>
+          {item.question} <ChevronsUpDown className='h-5 w-5 '/></CollapsibleTrigger>
+          <CollapsibleContent>
+              <div className='flex flex-col gap-2'>
+                <h2 className='text-re-500 p-2 border rounded-lg'><strong>Rating:</strong>{item.rating}</h2>
+                <h2 className='p-2 border rounded-lg bg-red-50 text-sm text-red-900'><strong>Your Answer:</strong>{item.UserAns}</h2>
+                <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer:</strong>{item.correctAns}</h2>
+                <h2 className='p-2 border rounded-lg bg-blue-50 text-sm text-blue-900'><strong>Feedback:</strong>{item.feedback}</h2>
+              </div>
+          </CollapsibleContent>
+        </Collapsible>
+      ))}
+
+      
+      <Button onClick={()=>router.replace('/dashboard')}>Back Home</Button>
+    </div>
   )
 }
 
-export default page
+export default Feedback;
